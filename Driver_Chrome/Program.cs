cbf2712bd02b1dea182e3c38cd49e8b88607041d
Katelynn Campbell-Bishop <klyncamp23@gmail.com>
2018-12-11 21:29:58 -0500

    Stock Symbols to the console

--- Driver_Chrome/Program.cs
+++ Driver_Chrome/Program.cs
@@ -13,7 +13,7 @@ namespace Driver_Chrome
         static void Main(string[] args)
         {
             // Create a driver instance for chromedriver
-            IWebDriver driver = new ChromeDriver(@"C:\Users\klync\Downloads\chromedriver_win32");
+            IWebDriver driver = new ChromeDriver(@"C:\Users\klync\Source\Repos\Driver_Chrome\Driver_Chrome\bin\Debug\netcoreapp2.2");
 
             //Navigate to google page
             driver.Navigate().GoToUrl("https://login.yahoo.com/?.src=finance&.intl=us&authMechanism=primary&done=https%3A%2F%2Ffinance.yahoo.com%2Fscreener%2Fpredefined&eid=100&add=1");
@@ -26,12 +26,12 @@ namespace Driver_Chrome
             driver.FindElement(By.Id("login-passwd")).SendKeys("" + Keys.Enter);
 
             driver.FindElement(By.XPath("//a[contains(text(), 'My Portfolio')]")).Click();
-            driver.FindElement(By.XPath("//a[contains(text(), 'Katelynns Portfolio')]")).Click();
+            driver.FindElement(By.XPath("//tr[@data-key='p_0']//td[1]")).Click();
 
             //Find the Search text box using xpath
             IList<IWebElement> stockNames = driver.FindElements(By.ClassName("_61PYt"));
 
-            for (int i = 0; i < stockNames.Count; i++)
+            for(int i = 0; i < stockNames.Count; i++)
             {
                 Console.WriteLine(stockNames[i].Text);
             }
